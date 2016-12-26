703e5c83df878a6093b01175f7887addbfeeace2
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2016-12-27 00:16:53 +0100

    Red. Add int2bin

--- src/BinStrTransm.hs
+++ src/BinStrTransm.hs
@@ -8,3 +8,6 @@ transmit xs = "Hello world!"
 bin2int :: [Bit] -> Int
 bin2int bits = sum [w * b | (w,b) <- zip weights bits]
                   where weights = iterate (*2) 1
+
+int2bin :: Int -> [Bit]
+int2bin = undefined
--- test/Tests.hs
+++ test/Tests.hs
@@ -6,10 +6,10 @@ import Test.Tasty.HUnit
 import BinStrTransm
 
 message = "Hello world!"
-b2i_1   = [1, 0, 1, 1]
-b2i_2   = [0, 0, 0, 0]
-b2i_3   = [1, 0, 0, 0]
-b2i_4   = [0, 0, 0, 1]
+b2i_1   = [1, 0, 1, 1] ; i2b_1 = 13
+b2i_2   = [0, 0, 0, 0] ; i2b_2 =  0
+b2i_3   = [1, 0, 0, 0] ; i2b_3 =  1
+b2i_4   = [0, 0, 0, 1] ; i2b_4 =  8
 
 binStrTransmSuite :: TestTree
 binStrTransmSuite =
@@ -19,14 +19,20 @@ binStrTransmSuite =
             (transmit message) @?= "Hello world!"
         ]
     , testGroup "bin2int"
-      [ testCase ("bin2int " ++ (show b2i_1) ++ " -> 13") $
-          (show $  bin2int            b2i_1) @?=    "13"
-      , testCase ("bin2int " ++ (show b2i_2) ++ " ->  0") $
-          (show $  bin2int            b2i_2) @?=     "0"
-      , testCase ("bin2int " ++ (show b2i_3) ++ " ->  1") $
-          (show $  bin2int            b2i_3) @?=     "1"
-      , testCase ("bin2int " ++ (show b2i_4) ++ " ->  8") $
-          (show $  bin2int            b2i_4) @?=     "8"      ]
+        [ testCase ("bin2int " ++ (show b2i_1) ++ " -> 13") $
+            (show $  bin2int            b2i_1) @?=    "13"
+        , testCase ("bin2int " ++ (show b2i_2) ++ " ->  0") $
+            (show $  bin2int            b2i_2) @?=     "0"
+        , testCase ("bin2int " ++ (show b2i_3) ++ " ->  1") $
+            (show $  bin2int            b2i_3) @?=     "1"
+        , testCase ("bin2int " ++ (show b2i_4) ++ " ->  8") $
+            (show $  bin2int            b2i_4) @?=     "8"      ]
+    , testGroup "int2bin"
+        [ testCase ("int2bin " ++ (show i2b_1) ++ "-> [1, 0, 1, 1]") $
+            (show $  int2bin            i2b_1) @?= "[1, 0, 1, 1]"
+        , testCase ("int2bin " ++ (show i2b_2) ++ "-> [0, 0, 0, 0]") $
+            (show $  int2bin            i2b_2) @?= "[0, 0, 0, 0]"
+        ]
     ]
 
 main = defaultMain binStrTransmSuite
